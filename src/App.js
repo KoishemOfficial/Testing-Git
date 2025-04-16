fdda2a2e37da1963b8f9ea3aa2e782268c346be2
Koishem <koishem.official@gmail.com>
2025-04-16 21:53:22 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import styles from '../style/header.module.css';
+
+function Header(){
+    return(
+        <header className={styles.test}>
+            <h1>Trying out React</h1>
+        </header>
+    );
+}
+
+export default Header;
